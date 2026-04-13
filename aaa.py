8b1983dac280b3e1305ad03c518965157a6e8afd
unknown <roshanad63@gmail.com>
2026-04-13 14:26:24 +0545

    Add; First Python Project

---
+++ aaa.py
@@ -0,0 +1,17 @@
+import keyword
+
+print(len(keyword.kwlist))
+print('hell')
+
+age = 2+17j #complex
+print(age.real, age.imag)
+print(id(age))
+print(type(age))
+
+grade = []#list
+print(type(grade))
+
+marks = {*()}#set
+print(type(marks))
+
+# indexing, slicing, scape character, string method
